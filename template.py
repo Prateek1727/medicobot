3a25d821dfedb3809b24d3ad534429aa20e414f8
prateek.anand23b@iiitg.ac.in <prateek.anand23b@iiitg.ac.in>
2025-05-19 23:53:59 +0530

    initial commit

---
+++ template.py
@@ -0,0 +1,41 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+list_of_files = [
+    "src/__init__.py",
+    "src/helper.py",
+    "src/ai_function.py",
+    "src/database.py",
+    "src/chatbot.py",
+    "tests/__init__.py",
+    "tests/test_chatbot.py",
+    ".env",
+    "setup.py",
+    "app.py",
+    "research/trials.ipynb",
+    "pytest.ini",
+    "test.py"
+    
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    else:
+        logging.info(f"{filename} is already exists")
