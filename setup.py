a101ea8448ab0f2e8e1fd85599c64b11908647f1
Prateek Anand <prateek.anand23b@iiitg.ac.in>
2025-05-23 20:36:36 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'Generative AI Project',
     version= '0.0.0',
-    author= 'Chinelo Nweke',
-    author_email= 'nwekechinelo25@yahoo.com',
+    author= 'Prateek Anand',
+    author_email= 'prateekanandcuriosity@gmail.com',
     packages= find_packages(),
     install_requires = []
 
